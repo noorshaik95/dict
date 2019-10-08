b0c5f239649e151c285445ee6fa92508f9f1caa9
NoorUllah Shaik <noor.shaik95@hotmail.com>
2019-10-08 19:43:00 +0530

    formatted the result of each command.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const config = require('config');
 const utils = require('./utils');
 const dict = require('./dict');
 let type, word;
+let definitions = [], antonyms = [], synonyms = [], examples = [];
+
 if (config.commandsWithWord.indexOf(argv._[0]) > -1) {
     if(!argv._[1]) {
         console.log("Invalid input");
@@ -18,39 +20,90 @@ if (config.commandsWithWord.indexOf(argv._[0]) > -1) {
 } else if (argv._[0] && !argv._[1]) {
     word = argv._[0];
 }
-let response;
-process(type, word);
-async function process(type, word = '') {
-    if(!type) {
+
+async function getDetails(type, word = '') {
+    let response;
+    try {
+        switch (type) {
+            case 'def':
+                response = await dict.getDefinition(word, config.extendUrl + utils.getExtendedUrl(type, word));
+                for(let ex of response.body) {
+                    definitions.push(ex.text);
+                }
+                return definitions;
+            case 'syn':
+            case 'ant':
+                response = await dict.getRelatedWord(word, config.extendUrl + utils.getExtendedUrl(type, word));
+                for(let data of response.body) {
+                    if(data.relationshipType === 'antonym') {
+                        antonyms = [...data.words];
+                    }
+                    if(data.relationshipType === 'synonym') {
+                        synonyms = [...data.words];
+                    }
+                }
+                return {antonyms, synonyms};
+            case 'ex':
+                response = await dict.getExample(word, config.extendUrl + utils.getExtendedUrl(type, word));
+                for(let ex of response.body.examples) {
+                    examples.push(ex.text);
+                }
+                return examples;
+            case 'play':
+                await getAllDetails(word);
+
+                break;
+            case 'dict':
+            default:
+                await getAllDetails(word);
+        }
+    } catch (e) {
+        console.error(`Error: ${e.error}`);
+        process.exit(1);
+    }
+}
+
+async function getAllDetails(word) {
+    let responses = [];
+    for(let type of config.commandsWithWord) {
+        if(type !== 'ant') {
+            responses.push(getDetails(type, word));
+        }
+    }
+    return await Promise.all(responses);
+}
+
+async function startApp() {
+    if((!type || type === 'play') && !word) {
         let response = await dict.getRandomWord();
         if(response.res.statusCode === 200) {
             word = response.body.word;
         }
     }
-    switch (type) {
-        case 'def':
-            response = await dict.getDefinition(word, config.extendUrl + utils.getExtendedUrl(type, word));
-            return response.body;
-        case 'syn':
-            response = await dict.getRelatedWord(word, config.extendUrl + utils.getExtendedUrl(type, word));
-            return response.body;
-        case 'ant':
-            response = await dict.getRelatedWord(word, config.extendUrl + utils.getExtendedUrl(type, word));
-            return response.body;
-        case 'ex':
-            response = await dict.getExample(word, config.extendUrl + utils.getExtendedUrl(type, word));
-            return response.body;
-        case 'play':
-            console.log('play');
-            break;
-        case 'dict':
-        default:
-            let responses = [];
-            for(let type of config.commandsWithWord) {
-                if(type !== 'ant') {
-                    responses.push(process(type, word));
-                }
+    let data = await getDetails(type, word);
+    if (config.commandsWithWord.indexOf(type) > -1) {
+        utils.printLine();
+        console.log(utils.printDetails(type, data, utils.preText(type, word)))
+    } else {
+        console.log(utils.preText('word', word));
+        utils.printLine();
+        for(let type of config.commandsWithWord) {
+            if(type === 'def') {
+                console.log(utils.printDetails(type, definitions, utils.preText(type, word)));
             }
-            console.log(await Promise.all(responses));
+            if(type === 'ex') {
+                console.log(utils.printDetails(type, examples, utils.preText(type, word)));
+            }
+            if(type === 'syn') {
+                console.log(utils.printDetails(type, {synonyms}, utils.preText(type, word)));
+            }
+            if(type === 'ant') {
+                console.log(utils.printDetails(type, {antonyms}, utils.preText(type, word)));
+            }
+            utils.printLine();
+        }
     }
+    process.exit(1);
 }
+
+startApp();
--- utils.js
+++ utils.js
@@ -21,6 +21,9 @@ module.exports = {
                 if(err) {
                     return reject(err);
                 }
+                if(res.statusCode !== 200) {
+                    return reject(body)
+                }
                 resolve({err, res, body});
             })
         });
@@ -28,5 +31,38 @@ module.exports = {
     getExtendedUrl(type, word) {
         return config[config.mappedUrl[type]].replace(/:word/g, word);
     },
-
+    printDetails(type, data, printData = '') {
+        if(['def', 'ex'].indexOf(type) > -1) {
+            return printData + '\n' + data.map((s, index) => {
+                return `${index + 1}. ${s}`;
+            }).join('\n');
+        }
+        if(['ant', 'syn'].indexOf(type) > -1) {
+            return printData + data[type === 'syn' ? 'synonyms' : 'antonyms'].join(', ');
+        }
+    },
+    preText(type = 'word', word) {
+        let string;
+        switch (type) {
+            case 'def':
+                string = 'Definition';
+                break;
+            case 'ex':
+                string = 'Example';
+                break;
+            case 'syn':
+                string = 'Synonym';
+                break;
+            case 'ant':
+                string = 'Antonym';
+                break;
+            case 'word':
+                string = 'Details';
+                break;
+        }
+        return `${string} for the word: ${word}`;
+    },
+    printLine() {
+        console.log('----------------------------------------------------------------');
+    }
 };
